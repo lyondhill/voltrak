4da66a810c9eafdbad2f669d29996fe2dd969931
lyon <lyondhill@gmail.com>
2012-10-25 20:59:20 -0600

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voltrak::Application.config.secret_token = '58827e55fe7bf64e586e4e0fc5f01cb97b79ac5a7c3629bdfb04fcd586f5fa9e72bc04d57a58bec253931b062bdc029775ca76f802b425dc9845515185bd50e2'
