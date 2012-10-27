f3b0c9d2636e7e662ae822b9cd72730d12844fe5
lyon <lyondhill@gmail.com>
2012-10-27 13:34:48 -0600

    awesome

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,6 +1,8 @@
 class Frame
   include Mongoid::Document
 
+  field :temperature, type: Integer
+
   has_many :cells
   belongs_to :plant
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,17 @@ if Rails.env.development?
     else
       unless o.errors.messages.include?(:transaction)
         puts " Error!".color(:red)
-        puts " #{o.errors.pretty_inspect}".color(:yellow)
+        puts " #{o.errors.pretty_inspect}".color(:red)
         exit 1
       else
-        puts " Complete!".color(:green)
+        puts " Complete!"
       end
     end
   end
 
   require 'rainbow'
 
-  Sickill::Rainbow.enabled = true
+  # Sickill::Rainbow.enabled = true
 
   Mongoid.logger = nil
   Mongoid::Config.purge!
@@ -37,9 +37,38 @@ if Rails.env.development?
   # end
 
   ###  -- BEGIN SEEDING DATABASE --
-  puts "\n\n\n--- Begin Seeding Database ---".color(:cyan).underline
+  puts "\n\n\n--- Begin Seeding Database ---"
+
+  puts "Generating a plant"
+
+  p = Plant.create(name: "Singapore")
+
+  puts "generationg frames"
+  Plant.all.each do |plant|
+    rand(2..5).times do
+      plant.frames.create(temperature: rand(1..100))
+      plant.frames.create(temperature: rand(1..100))
+    end
+  end
+
+  puts "generate cells"
+  Frame.all.each do |frame|
+    rand(1..5).times do
+      frame.cells.create(status: [:errored, :active, :inactive][rand(0..2)])
+    end
+  end
+
+
+  puts "generate reports"
+  Cell.all.each do |cell|
+    week_epoc = (Time.now - 1.week.ago)
+    1000.times do |num|
+      time = (Time.now - ((week_epoc / 1000) + (num * (week_epoc / 1000)) ))
+      cell.reports.create(report_time: time, voltage: rand(0.0..30.0).round(2))
+    end
+  end
 
   # seed stuff here
 
-  puts "\n!!! Database Seed Status: #{'SUCCESS'.color(:green).bright} !!!".color(:cyan)
+  puts "\n!!! Database Seed Status: #{'SUCCESS'.color(:green).bright} !!!"
 end
